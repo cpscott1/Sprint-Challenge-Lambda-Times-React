f9901c4f2eded9e4b69757bcac9f511880020d34
cpscott1 <thacam18@gmail.com>
2019-03-23 13:02:30 -0500

    fixed some functionality

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -25,6 +25,9 @@ export default class Content extends Component {
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState({
+      selected: tab
+    })
   };
 
   filterCards = () => {
@@ -40,7 +43,13 @@ export default class Content extends Component {
           of the items from cardData.
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+      if(this.state.selected.includes('all')) {
+        return this.state.cards;
+      } else {
+        return this.state.cards.filter(card => {
+          return card.tab === this.state.selected
+        })
+      }
   };
 
   render() {
@@ -51,7 +60,10 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs tabs={this.state.tabs}
+          selectedTab={this.state.selected}
+          selectTabHandler={this.changeSelected}
+         />
         <Cards cards={this.filterCards()} />
       </div>
     );
